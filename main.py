fdd34c55c6c67fc588f0a421ed93e760eb476af8
philipreese <pbreese42@gmail.com>
2025-06-25 00:13:54 -0400

    change to allow list of messages for prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -14,16 +15,24 @@ def main():
 
     client = genai.Client(api_key=api_key)
     prompt = " ".join(sys.argv[1:])
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
     response = client.models.generate_content(  # type: ignore
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
 
     if not response.usage_metadata:
         print("No response received.")
         return
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     print("Response:")
     print(response.text)
 
