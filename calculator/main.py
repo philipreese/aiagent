ef785cb3e719269d68881bd3383b36ce94859f7e
philipreese <pbreese42@gmail.com>
2025-06-25 00:52:23 -0400

    add calculator test project

---
+++ calculator/main.py
@@ -0,0 +1,29 @@
+# main.py
+
+import sys
+from pkg.calculator import Calculator
+from pkg.render import render
+
+
+def main():
+    calculator = Calculator()
+    if len(sys.argv) <= 1:
+        print("Calculator App")
+        print('Usage: python main.py "<expression>"')
+        print('Example: python main.py "3 + 5"')
+        return
+
+    expression = " ".join(sys.argv[1:])
+    try:
+        result = calculator.evaluate(expression)
+        if result is None:
+            print("Error in calculation")
+            return
+        to_print = render(expression, result)
+        print(to_print)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
