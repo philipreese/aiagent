91e540ce61a2744638d5d1c9f6829583c220a246
philipreese <pbreese42@gmail.com>
2025-06-25 00:09:13 -0400

    allow for user input prompt

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    if len(sys.argv) == 1:
+        print('Usage: python main.py "<prompt>"')
+        sys.exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-
+    prompt = " ".join(sys.argv[1:])
     response = client.models.generate_content(  # type: ignore
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=prompt,
     )
 
     if not response.usage_metadata:
