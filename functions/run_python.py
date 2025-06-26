beb837d59843d87c44d04feca10d370004af913f
philipreese <pbreese42@gmail.com>
2025-06-26 00:04:06 -0400

    finish function declarations

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,8 +1,11 @@
 import os
 import subprocess
+from google.genai import types
 
 
-def run_python_file(working_directory: str, file_path: str) -> str:
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
     working_directory_abs = os.path.abspath(working_directory)
     abs_path = os.path.abspath(os.path.join(working_directory_abs, file_path))
 
@@ -16,8 +19,11 @@ def run_python_file(working_directory: str, file_path: str) -> str:
         return f'Error: "{file_path}" is not a Python file.'
 
     try:
+        commands = ["python", abs_path]
+        if args:
+            commands.extend(args)
         result = subprocess.run(
-            ["python", abs_path],
+            commands,
             cwd=working_directory_abs,
             timeout=30,
             capture_output=True,
@@ -37,3 +43,27 @@ def run_python_file(working_directory: str, file_path: str) -> str:
         return "\n".join(output)
     except Exception as e:
         return f"Error: executing Python file: {e}"
+
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a python file within the working directory and returns the output from the interpreter.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
